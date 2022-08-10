f4d1f2e1e6a24c713099d63fbd25dc3f8c539ba5
pasparo <piero.gasparotto@psi.ch>
2022-08-10 15:42:09 +0200

    Adding the source code directly as a tgz

--- fast_indexer_dev/perf-prep.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo sh -c 'echo -1 >/proc/sys/kernel/perf_event_paranoid'
-sudo sh -c 'echo 0 > /proc/sys/kernel/kptr_restrict'
-sudo sh -c 'echo 0 > /proc/sys/kernel/yama/ptrace_scope' 

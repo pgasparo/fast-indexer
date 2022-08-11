eaf9eb3cb9285657babde1c59cc8645f92cd3a4d
hcs <hans-christian.stadler@psi.ch>
2022-08-11 10:54:00 +0200

    Better library handling

--- fast_indexer_dev/test.sh
+++ fast_indexer_dev/test.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
 ROOT=${ROOT:-.}
-EXE=(${EXE:-${ROOT}/test-build/xgandalf})
+EXE=${EXE:-${ROOT}/test-build/xgandalf}
+LIBDIR=$(readlink -f $(dirname $(find ${ROOT}/xgandalf-install/lib -name libxgandalf.so)))
 
 if [[ ! -x ${EXE[0]} ]]; then
 	echo "$EXE not found, execute from root dir or set ROOT variable"
 	exit 1
 fi
 
-if [[ -z "$LD_LIBRARY_PATH" ]]; then
-	LD_LIBRARY_PATH=${ROOT}/xgandalf-build
-else
-	LD_LIBRARY_PATH+=:${ROOT}/xgandalf-build
+if ! printenv LD_LIBRARY_PATH | grep -s xgandalf &>/dev/null; then
+        if [[ -d ${LIBDIR} ]]; then
+                if [[ -z "$LD_LIBRARY_PATH" ]]; then
+                        LD_LIBRARY_PATH=${LIBDIR}
+                else
+                        LD_LIBRARY_PATH+=:${LIBDIR}
+                fi
+        fi
 fi
+
+export LD_LIBRARY_PATH
 
 eval ${EXE[@]} "$@"
